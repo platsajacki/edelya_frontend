90f3fff30edce662276175e430a9976e1f4ea418
Vyacheslav Menyukhov <plasajacki@gmail.com>
2026-09-13 14:28:45 +0300

    Add og banner.

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -6,4 +6,13 @@ envsubst '${VITE_API} ${VITE_TELEGRAM_BOT}' \
   < /usr/share/nginx/html/config.js.template \
   > /usr/share/nginx/html/config.js
 
+VITE_SITE_URL="${VITE_SITE_URL:-https://edelya.corpdi.com}"
+VITE_SITE_URL="${VITE_SITE_URL%/}"
+export VITE_SITE_URL
+
+envsubst '${VITE_SITE_URL}' \
+  < /usr/share/nginx/html/index.html \
+  > /usr/share/nginx/html/index.html.tmp
+mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
+
 exec nginx -g "daemon off;"
